22874b14b0528785f4d0f584aa1eb2d490bb0381
ThalKod <tmarcelin@thal.tech>
2018-09-06 14:53:49 -0400

    map headline reducers to props

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,20 +5,34 @@ import { startFetchHeadline } from "../actions/index";
 
 class HomePage extends React.Component{
 
+    state = {
+        headline: this.props.headline
+    }
+
     componentDidMount = ()=>{
         this.props.fetchHeadline();
     }
+
     render(){
         return (
             <div>
                 <Header />
+                <div className="main-container">
+                    {this.props.headline.slice(0,3).map((article)=>{
+                        return <p>{article.title}</p>
+                    })}
+                </div>
             </div>
         )
     }
 }
 
 const mapDispatchToProps = (dispatch)=>({
-    fetchHeadline: ()=> dispatch(startFetchHeadline())
+    fetchHeadline: ()=> dispatch(startFetchHeadline()),
 });
 
-export default connect(null, mapDispatchToProps)(HomePage);
+const mapStateToProps = (state)=>({
+    headline: state.articlesHeadline
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
