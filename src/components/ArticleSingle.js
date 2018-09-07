948a653f15bdb87182d1696985b18c163f407861
ThalKod <tmarcelin@thal.tech>
2018-09-07 12:49:07 -0400

    populate ArticleSingle component

--- src/components/ArticleSingle.js
+++ src/components/ArticleSingle.js
@@ -1,11 +1,31 @@
 import React from "react";
 
-export class ArticleSingle extends React.Component{
+export default class ArticleSingle extends React.Component{
     render(){
         return (
-            <div>
-                
-            </div>
+            <div class="columns column-4">
+                <div class="post-box" style={{ backgroundImage: this.props.urlToImage }}>
+                    <div class="post-overlay">
+                        <a href="#" class="post-category" title="Title of blog post" rel="tag">Technology</a>
+                        <h3 class="post-title">{this.props.title}</h3>
+                        <div class="post-meta">
+                            <div class="post-meta-author-avatar">
+                                <img alt="avatar" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" class="avatar" height="24" width="24" />
+                            </div>
+                            <div class="post-meta-author-info">
+                                <span class="post-meta-author-name">
+                                    <a href="#" title="Posts by John Doe" rel="author">{this.props.source.name}</a>
+                                </span>
+                                <span class="middot">·</span>
+                                <span class="post-meta-date">
+                                    <abbr class="published updated" title="December 4, 2017">12 November 2018</abbr>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#" class="post-overlayLink"></a>
+                </div>
+         </div>
         )
     }
 }
--- src/components/HeadlineGroup.js
+++ src/components/HeadlineGroup.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import ArticleSingle from "./ArticleSingle";
 
-export default class HeadlineGroup extends React.Component{
+class HeadlineGroup extends React.Component{
+
+
     render(){
         return (
             <div className="main-highlight">
                 <div className="highlight-carousel grid-posts">
                     <div className="owl-carousel">
                         <div className="grid-rows">
-                        
+                            {
+                                this.props.headline.slice(0,3).map((article)=>{
+                                    return <ArticleSingle {...article}  />
+                                })
+                            }
                         </div>
                     </div>
                 </div>
@@ -15,3 +23,9 @@ export default class HeadlineGroup extends React.Component{
         )
     }
 }
+
+const mapStateToProps = (state)=>({
+    headline:  state.articlesHeadline,
+});
+
+export default connect(mapStateToProps)(HeadlineGroup);
