a241b36df671917055956f576459c8fb249799dc
ThalKod <tmarcelin@thal.tech>
2018-09-11 12:54:18 -0400

    add ContentBody component

---
+++ src/components/ContentBody.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class ContentBody extends React.Component{
+    render(){
+        return (
+            <div className="content-body">
+                Content Body
+            </div>
+        )
+    }
+}
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import ContentBody from "./ContentBody";
+
 export default class MainContent extends React.Component{
     render(){
         return (
             <div className="main-content">
                 <div className="main-content-wrapper">
-                    
+                    <ContentBody />
                 </div>
             </div>
         )
