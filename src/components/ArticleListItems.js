bfc3e0c3f943802008272818940ffde8d2f4a3ba
ThalKod <tmarcelin@thal.tech>
2018-09-12 11:29:27 -0400

    add ArticleListItems component

---
+++ src/components/ArticleListItems.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class ArticleListItems extends React.Component {
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
--- src/components/ContentBody.js
+++ src/components/ContentBody.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+import ArticleListItems from "./ArticleListItems";
+
 export default class ContentBody extends React.Component{
     render(){
         return (
             <div className="content-body">
-                Content Body
+                <div className="content-timeline">
+                    <div class="post-list-header">
+                        <span class="post-list-title">Latest News</span>
+                    </div>
+                    <ArticleListItems />
+                </div>
             </div>
         )
     }
