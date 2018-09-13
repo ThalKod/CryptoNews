c0b80464c1077743b10a607ad4b4c76d72877c6d
ThalKod <tmarcelin@thal.tech>
2018-09-13 15:09:02 -0400

    add NotFoundPage component content

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -4,13 +4,17 @@ import { Link } from "react-router-dom";
 export default class NotFoundPage extends React.Component{
     render(){
         return (
-            <div>
-                <p>404 - No Found Page</p>
-                <Link to="/">
-                    <a>Homepage</a>
-                </Link>
+            <div class="main-container">
+                <div class="error-container">
+                    <div class="error-code">
+                        404
+                    </div>
+                    <h1>Sorry, but nothing exists here.</h1>
+                    <Link to="/">
+                        <a class="go-home-link">Go back</a>
+                    </Link>
+                </div>
             </div>
-            
         )
     }
 }
